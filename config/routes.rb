23dc69db43b572cdcfd000d6b37fba84795cedd6
Kristen McCabe <kmccabe3@gmail.com>
2014-11-19 12:04:16 -0800

    organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # Application home
-  get "/", to: "home#index", as: :root
+  get "/",                        to: "home#index",            as: :root
 
   # User routes
   get "/users",                   to: "users#index",           as: :users
@@ -14,14 +14,14 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
 
   # VIMEO CONTROLLER ROUTES
-  get 'vimeo/index', to: "vimeo#index", as: :vimeo
-  get 'vimeo/search', to: "vimeo#search", as: :vimeo_search
-  get 'vimeo/new', to: "vimeo#new"
-  put 'vimeo/create', to: "vimeo#create"
-  get 'vimeo/update', to: "vimeo#update"
-  patch 'vimeo/edit', to: "vimeo#edit"
-  delete 'vimeo/destroy', to: "vimeo#destroy"
-  get 'vimeo/show/:uid', to: "vimeo#show"
+  get 'vimeo/index',              to: "vimeo#index",           as: :vimeo
+  get 'vimeo/search',             to: "vimeo#search",          as: :vimeo_search
+  get 'vimeo/new',                to: "vimeo#new"
+  put 'vimeo/create',             to: "vimeo#create"
+  get 'vimeo/update',             to: "vimeo#update"
+  patch 'vimeo/edit',             to: "vimeo#edit"
+  delete 'vimeo/destroy',         to: "vimeo#destroy"
+  get 'vimeo/show/:uid',          to: "vimeo#show"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
