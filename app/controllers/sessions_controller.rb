e90c0e31a17eddd0d7997d7816a7469210b2f6b2
Rachel <rmoshier@gmail.com>
2014-11-18 16:54:51 -0800

    we didn't need params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,21 +5,23 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     @a = auth_hash["uid"]
 
-    @user = User.new(params.require(:user).permit(:github_uid))
+    # @user = User.new(params.require(:user).permit(:github_uid))
 
     provider = auth_hash["provider"]
 
     if provider == 'github'
-      User.create(github_uid: @a)
-      session[:id] = User.last[:id]
+      c = User.create(github_uid: @a)
+      session[:user_id] = c.id
     elsif provider == 'twitter'
-      User.create(twitter_uid: @a)
+      c = User.create(twitter_uid: @a)
+      session[:user_id] = c.id
     elsif provider == 'vimeo'
       User.create(vimeo_uid: @a)
+      session[:user_id] = c.id
     else
       User.create(insta_uid: @a)
+      session[:user_id] = c.id
     end
-    raise
   end
 
 end
