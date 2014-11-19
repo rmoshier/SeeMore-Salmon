eecb149403449f7e0566ddb5d41e3d38900231b8
Rachel <rmoshier@gmail.com>
2014-11-18 16:46:04 -0800

    got sessions to render

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
 
   def create
+
     auth_hash = request.env['omniauth.auth']
     @a = auth_hash["uid"]
 
+    @user = User.new(params.require(:user).permit(:github_uid))
+
     provider = auth_hash["provider"]
+
     if provider == 'github'
       User.create(github_uid: @a)
+      session[:id] = User.last[:id]
     elsif provider == 'twitter'
-      User.create(twitter_uid: auth_hash["uid"])
+      User.create(twitter_uid: @a)
     elsif provider == 'vimeo'
-      User.create(vimeo_uid: auth_hash["uid"])
+      User.create(vimeo_uid: @a)
     else
-      User.create(insta_uid: auth_hash["uid"])
+      User.create(insta_uid: @a)
     end
     raise
   end
