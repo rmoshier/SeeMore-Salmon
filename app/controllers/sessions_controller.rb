89b58f8cc21b79650d7bb96abbce9df1294437be
Rachel <rmoshier@gmail.com>
2014-11-19 11:02:57 -0800

    clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,42 +4,36 @@ class SessionsController < ApplicationController
 
     auth_hash = request.env['omniauth.auth']
     @a = auth_hash["uid"]
+    provider = auth_hash[:provider]
 
     # @user = User.new(params.require(:user).permit(:github_uid))
 
-    login = User.find_by_auth_hash[:provider]_uid: request.env["omniauth.auth"]["uid"])
-    if login.empty?
-      User.create((auth_hash[:provider]): request.env["omniauth.auth"][:uid])
+    # login = User.find_by_auth_hash[:provider]_uid: request.env["omniauth.auth"]["uid"])
+    # if login.empty?
+    #   User.create((auth_hash[:provider]): request.env["omniauth.auth"][:uid])
+    # end
+
+    if provider == 'github'
+      c = User.create(github_uid: @a)
+      session[:user_id] = c.id
+    elsif provider == 'twitter'
+      c = User.create(twitter_uid: @a)
+      session[:user_id] = c.id
+    elsif provider == 'vimeo'
+      User.create(vimeo_uid: @a)
+      session[:user_id] = c.id
+    else
+      User.create(insta_uid: @a)
+      session[:user_id] = c.id
     end
-
-    # if provider == 'github'
-    #   c = User.create(github_uid: @a)
-    #   session[:user_id] = c.id
-    # elsif provider == 'twitter'
-    #   c = User.create(twitter_uid: @a)
-    #   session[:user_id] = c.id
-    # elsif provider == 'vimeo'
-    #   User.create(vimeo_uid: @a)
-    #   session[:user_id] = c.id
-    # else
-    #   User.create(insta_uid: @a)
-    #   session[:user_id] = c.id
-
-    raise
   end
 
+  def destory
+    session[:id] = nil
+    redirect_to root_path
+  end
 
-
-    def destory
-      session[:id] = nil
-      redirect_to root_path
-    end
 end
-
-
-
-
-
 
 #login = User.where(tostring: request.env["omniauth.auth"][:uid])
 #if login.empty?
@@ -48,3 +42,5 @@ end
 #push into model unloginuser = User.new
 #then start a session
 #REDIRECT!
+
+#could use Bookis' fix:  User.find_by("diu".reverse => 1234)
