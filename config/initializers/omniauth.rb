8eda3c622426b915767e83a6b1539b96a9016a69
Kristen McCabe <kmccabe3@gmail.com>
2014-11-18 16:58:03 -0800

    add twitter provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-
-  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
-  provider :vimeo, ENV["VIMEO_CLIENT_ID"], ENV["VIMEO_CLIENT_SECRET"]
+  provider :github,  ENV["GITHUB_CLIENT_ID"],    ENV["GITHUB_CLIENT_SECRET"]
+  provider :vimeo,   ENV["VIMEO_CLIENT_ID"],     ENV["VIMEO_CLIENT_SECRET"]
+  provider :twitter, ENV["TWITTER_CONSUMER_ID"], ENV["TWITTER_CONSUMER_SECRET"]
 end
