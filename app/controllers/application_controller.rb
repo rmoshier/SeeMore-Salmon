e90e734c22baf18f15641e6f64cdc56885a19a5d
Calla <callapatel@gmail.com>
2014-11-19 11:58:51 -0800

    got the HP working again by deleting the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,53 +3,53 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-    auth_hash = request.env['omniauth.auth']
-    @a = auth_hash["uid"]
-    @provider = auth_hash[:provider]
-
-  def existing_user
-    if @provider == 'github'
-      c = User.find(githib_uid: @a)
-      session[:user_id] = c.id
-    elsif @provider == 'twitter'
-      c = User.find(twitter_uid: @a)
-      session[:user_id] = c.id
-    elsif @provider == 'vimeo'
-      c = User.find(vimeo_uid: @a)
-      session[:user_id] = c.id
-    else
-      c = User.find(insta_uid: @a)
-      session[:user_id] = c.id
-    end
-  end
-
-  def require_login
-
-
-    # @user = User.new(params.require(:user).permit(:github_uid))
-
-    # login = User.find_by_auth_hash[:@provider]_uid: request.env["omniauth.auth"]["uid"])
-    # if login.empty?
-    #   User.create((auth_hash[:@provider]): request.env["omniauth.auth"][:uid])
-    # end
-
-    if @provider == 'github'
-      c = User.create(github_uid: @a)
-      session[:user_id] = c.id
-    elsif @provider == 'twitter'
-      c = User.create(twitter_uid: @a)
-      session[:user_id] = c.id
-    elsif @provider == 'vimeo'
-      User.create(vimeo_uid: @a)
-      session[:user_id] = c.id
-    else
-      User.create(insta_uid: @a)
-      session[:user_id] = c.id
-    end
-
-  end
-
-  # need to add if they alredy exist
-
-
+#     auth_hash = request.env['omniauth.auth']
+#     @a = auth_hash["uid"]
+#     @provider = auth_hash[:provider]
+#
+#   def existing_user
+#     if @provider == 'github'
+#       c = User.find(githib_uid: @a)
+#       session[:user_id] = c.id
+#     elsif @provider == 'twitter'
+#       c = User.find(twitter_uid: @a)
+#       session[:user_id] = c.id
+#     elsif @provider == 'vimeo'
+#       c = User.find(vimeo_uid: @a)
+#       session[:user_id] = c.id
+#     else
+#       c = User.find(insta_uid: @a)
+#       session[:user_id] = c.id
+#     end
+#   end
+#
+#   def require_login
+#
+#
+#     # @user = User.new(params.require(:user).permit(:github_uid))
+#
+#     # login = User.find_by_auth_hash[:@provider]_uid: request.env["omniauth.auth"]["uid"])
+#     # if login.empty?
+#     #   User.create((auth_hash[:@provider]): request.env["omniauth.auth"][:uid])
+#     # end
+#
+#     if @provider == 'github'
+#       c = User.create(github_uid: @a)
+#       session[:user_id] = c.id
+#     elsif @provider == 'twitter'
+#       c = User.create(twitter_uid: @a)
+#       session[:user_id] = c.id
+#     elsif @provider == 'vimeo'
+#       User.create(vimeo_uid: @a)
+#       session[:user_id] = c.id
+#     else
+#       User.create(insta_uid: @a)
+#       session[:user_id] = c.id
+#     end
+#
+#   end
+#
+#   # need to add if they alredy exist
+#
+#
 end
