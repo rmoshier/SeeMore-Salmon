e54a827256241c8bde47058664fbbd8585acb152
Calla <callapatel@gmail.com>
2014-11-18 16:05:32 -0800

    routes and starting sessions controller, not taking in UID

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,19 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    raise
+    auth_hash["uid"]
+
+    provider = auth_hash["provider"]
+    if provider == 'github'
+      User.create[github_uid: auth_hash["uid"]]
+    elsif provider == 'twitter'
+      User.create[twitter_uid: auth_hash["uid"]]
+    elsif provider == 'vimeo'
+      User.create[vimeo_uid: auth_hash["uid"]]
+    else
+      User.create[insta_uid: auth_hash["uid"]]
+    end
+    raise 
   end
-  
+
 end
