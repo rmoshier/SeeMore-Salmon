855b9cd4743c6fa3dd5dce89ed1fcc812577cc67
Calla <callapatel@gmail.com>
2014-11-19 11:46:15 -0800

    new keys for me

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
   provider :vimeo, ENV["VIMEO_CLIENT_ID"], ENV["VIMEO_CLIENT_SECRET"]
+  provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"]
 end
