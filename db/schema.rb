214b4574b45af3324253cdcf45acf95cf0e08f0f
Kristen McCabe <kmccabe3@gmail.com>
2014-11-20 14:15:44 -0800

    rename Provider column provider as name

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120202848) do
+ActiveRecord::Schema.define(version: 20141120221236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "providers", force: true do |t|
     t.integer  "user_id"
-    t.string   "provider"
+    t.string   "name"
     t.string   "uid"
     t.string   "access_token"
     t.string   "access_token_secret"
