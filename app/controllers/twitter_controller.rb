582fd0657ae3f1f18cf0332af1b7dd0300257c86
Kristen McCabe <kmccabe3@gmail.com>
2014-11-21 00:03:48 -0800

    add find_provider, create_twitter_client, new, and show methods

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,20 +1,37 @@
 class TwitterController < ApplicationController
 
   def new
-    provider = Provider.find_by_user_id(session[:user_id])
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_API_KEY"]
-      config.consumer_secret     = ENV["TWITTER_API_SECRET"]
-      config.access_token        = provider.token
-      config.access_token_secret = provider.secret
-    end
+    find_provider
+    create_twitter_client
   end
 
+  # secure?
+  # put this in a separate controller?
   def show
+    find_provider
+    @search_results = create_twitter_client.user_search(params[:username])
 
   end
 
   def create
+    find_provider
+    raise
+  end
+
+  private
+
+  def find_provider
+    @provider = Provider.find_by_user_id(session[:user_id])
+  end
+
+  # Is this OK? Better way to do this with OmniAuth?
+  def create_twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_API_KEY"]
+      config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+      config.access_token        = @provider.token
+      config.access_token_secret = @provider.secret
+    end
   end
 end
 
