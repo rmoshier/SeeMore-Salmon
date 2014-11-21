d0012d457223b66756f8fed6d0195a41ffd1f281
Kristen McCabe <kmccabe3@gmail.com>
2014-11-20 20:45:44 -0800

    keep fiddling with twitter

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,10 +1,16 @@
 class TwitterController < ApplicationController
 
   def new
+    provider = Provider.find_by_user_id(session[:user_id])
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_API_KEY"]
+      config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+      config.access_token        = provider.token
+      config.access_token_secret = provider.secret
+    end
   end
 
   def show
-    # Twitter::REST
 
   end
 
