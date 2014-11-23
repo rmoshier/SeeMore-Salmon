e43aa822dfa8c849f3f1b00fbbf19289ae478a03
Calla <callapatel@gmail.com>
2014-11-23 11:20:06 -0800

    refactored code for instagram

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,9 +1,5 @@
-require 'instagram'
-
 class InstagramController < ApplicationController
-
-
-
+  
   def index
     # client = Instagram.client(:access_token => session[:access_token])
   end
@@ -18,26 +14,24 @@ class InstagramController < ApplicationController
   end
 
   def search
-    client = Instagram.client(:access_token => session[:access_token])
+    # client = Instagram.client(:access_token => session[:access_token])
     create_instagram_client
     find_provider
     @srch = params[:q]
     @results = Instagram.client.user_search(@srch)
     # render :instagram_results
-
-
   end
 
 
   def create
-    @instagram_username = params['username']
-    find_provider
-    @user = User.find(session[:user_id])
-    @provider = params[:provider]
-    @name = params['search']
-    client = Instagram.client(:access_token => session[:access_token])
-    @search_results = create_instagram_client.user_search(params[:name])
-    @results = client.user_search(params[:username], { count: 10 })
+  #   @instagram_username = params['username']
+  #   find_provider
+  #   @user = User.find(session[:user_id])
+  #   @provider = params[:provider]
+  #   @name = params['search']
+  #   client = Instagram.client(:access_token => session[:access_token])
+  #   @search_results = create_instagram_client.user_search(params[:name])
+  #   @results = client.user_search(params[:username], { count: 10 })
   end
 
   private
