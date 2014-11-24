539596010d7f4b414861b7e227f75f5e311f9607
Rachel <rmoshier@gmail.com>
2014-11-24 11:17:04 -0800

    controller work

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -1,2 +1,25 @@
 class GithubsController < ApplicationController
+
+  def new
+    # find_provider
+    create_github_client
+  end
+
+  private
+
+  # def find_provider
+  #   @provider = Provider.find_by_user_id(session[:user_id])
+  # end
+
+  def create_github_client
+    # Provide authentication credentials
+    Octokit.configure do |c|
+      c.login = 'defunkt'
+      c.password = 'c0d3b4ssssss!'
+    end
+
+    # Fetch the current user
+    Octokit.user
+
+  end
 end
