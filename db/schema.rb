6cfadb8f01bb4821367fb69f17d5157f52b233ba
Kristen McCabe <kmccabe3@gmail.com>
2014-11-24 12:21:03 -0800

    update feed model and create Subscriptions join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124193453) do
+ActiveRecord::Schema.define(version: 20141124201412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20141124193453) do
     t.integer  "user_id"
     t.string   "provider"
     t.string   "username"
-    t.integer  "subscribed_uid"
+    t.integer  "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20141124193453) do
     t.string   "secret"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "subscriptions", id: false, force: true do |t|
+    t.integer "feed_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
