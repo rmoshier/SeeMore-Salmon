635923defa9be74e769c9697218ca02314f57743
Rachel <rmoshier@gmail.com>
2014-11-25 10:38:47 -0800

    refactored githubs controller

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -3,26 +3,11 @@ class GithubsController < ApplicationController
   def new
     find_provider
     create_github_client
-    # raise
   end
 
   def show
-    @client = Octokit::Client.new(:access_token => find_provider.token)
+    create_github_client
     @search_results = @client.search_users("#{params[:q]} in:login")
-    # raise
-  end
-
-# Octokit.search_users("book in:login")
-
-  def search
-    find_provider
-    # raise
-    # client = Instagram.client(:access_token => session[:access_token])
-    # create_github_client
-    # find_provider
-    # @srch = params[:q]
-    # @results = Github.client.user_search(@srch)
-    # render :instagram_results
   end
 
   private
@@ -32,13 +17,7 @@ class GithubsController < ApplicationController
   end
 
   def create_github_client
-    # @client = Octokit::Client.new(:access_token => find_provider.token)
-    # line 25 is for if we needed to be authenticated
+    @client = Octokit::Client.new(:access_token => find_provider.token)
   end
 
 end
-
-# Octokit.search_users("book in:login")
-# book = get it from the form
-# searches the whole code and page
-# shorthand for line 25
