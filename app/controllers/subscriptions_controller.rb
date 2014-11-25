9686e46da42e9b777872e5000037505c7de74ab7
Kristen McCabe <kmccabe3@gmail.com>
2014-11-25 00:19:30 -0800

    add subscriptions#create

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,15 @@
 class SubscriptionsController < ApplicationController
 
   def create
+    @feed = Feed.find_by_uid(params[:feed_uid])
+
+    if @feed
+      current_user.subscriptions.create(feed_id: @feed.id)
+    else
+      current_user.feeds.create(provider: "twitter",
+                                username: "test",
+                                uid: params[feed_uid],
+                                )
+    end
   end
 end
