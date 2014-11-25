a9067ee394c04435b84f2d36640a0451036ddf76
Kristen McCabe <kmccabe3@gmail.com>
2014-11-25 01:02:15 -0800

    comment out js

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -1,20 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(function() {
-  $( "a.twitter.subscribe-link" ).click(function( event ) {
-
-    event.preventDefault();
-
-    var twitterHandle = $( this ).innerHTML;
-    var url = $( this ).href;
-
-    $.ajax( url, {
-      type: "POST",
-      success: function (data) {
-        // div.addClass("chosen")
-      console.log(data);
-      }
-    });
-  });
-});
+// $(function() {
+//   $( "a.twitter.subscribe-link" ).click(function( event ) {
+//
+//     event.preventDefault();
+//
+//     var twitterHandle = $( this ).innerHTML;
+//     var url = $( this ).href;
+//
+//     $.ajax( url, {
+//       type: "POST",
+//       success: function (data) {
+//         // div.addClass("chosen")
+//       console.log(data);
+//       }
+//     });
+//   });
+// });
