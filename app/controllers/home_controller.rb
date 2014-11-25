95792b4eb5dca64676d4b7027e2f45a9da3b5cd0
Kristen McCabe <kmccabe3@gmail.com>
2014-11-25 09:40:16 -0800

    comment out some code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,22 +4,30 @@ class HomeController < ApplicationController
 
   def index
     # Gather all subscriptions for user
-    feeds = Feed.where(user_id: session[:user_id])
+    # change this
+    # git checkout commit# file_path then hash from the
+    # git checkout commit# db/migrate/
+    # rake db:drop dp:create db:migrate
+
+
+
+
+    #feeds = Feed.where(uid: session[:user_id])
     # Get vimeo subscriptions from all subscriptions
-    vimeo_feeds = feeds.select {|feed| feed.provider == "vimeo"}
+    #vimeo_feeds = feeds.select {|feed| feed.provider == "vimeo"}
     # Get vimeo uid's for searching api
-    vimeo_uids = vimeo_feeds.collect {|feed| feed.uid.to_s}
+    #vimeo_uids = vimeo_feeds.collect {|feed| feed.uid.to_s}
     # Searching vimeo api with vimeo uids, returns parsed vimeo json obejct
-    @vimeo_vids = vimeo_uids.collect {|uid|   HTTParty.get("http://vimeo.com/api/v2/" + uid + "/videos.json")}
+    #@vimeo_vids = vimeo_uids.collect {|uid|   HTTParty.get("http://vimeo.com/api/v2/" + uid + "/videos.json")}
 
     ###### THE ACTUAL CODE ######
-    @filtered_videos = @vimeo_vids.collect do |httparty|
-      httparty.collect do |video_object|
-        filter_video_response(video_object)
-      end
-    end
+    #@filtered_videos = @vimeo_vids.collect do |httparty|
+    #  httparty.collect do |video_object|
+    #    filter_video_response(video_object)
+    #  end
+    #end
 
-    @filtered_videos = @filtered_videos.flatten
+    #@filtered_videos = @filtered_videos.flatten
 
     #raise
 
