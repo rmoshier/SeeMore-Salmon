74451f76bda3a221ef4fa47f65717cedeb3e195c
Kristen McCabe <kmccabe3@gmail.com>
2014-11-25 17:56:50 -0800

    add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141125011744) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "feeds", force: true do |t|
