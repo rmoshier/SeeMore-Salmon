296cb7e8cb92a58289169b20f11afd4094f5da06
Kristen McCabe <kmccabe3@gmail.com>
2014-11-30 14:27:18 -0800

    sort posts by posted_time, prevent duplicate posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
       # put this in a new method
       @client.user_timeline(feed.uid.to_i).each do |tweet|
         # try find_or_create_by(uid: blahblahid)
-        # if tweet.id.nil?
+        if tweet.id.nil?
         feed.posts.create(author_name: tweet.user.name,
                           author_handle: tweet.user.handle,
                           author_profile_pic: tweet.user.profile_image_uri.to_s,
@@ -23,6 +23,7 @@ class HomeController < ApplicationController
                           uid: tweet.id,
                           posted_time: tweet.created_at
         )
+        end
         end
       end
 
