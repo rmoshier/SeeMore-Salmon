da0df367ab0eb0af8a7550b35f033d9d23785b37
Kristen McCabe <kmccabe3@gmail.com>
2014-12-01 12:28:38 -0800

    add passing test for twitter#new

--- spec/controllers/twitter_controller_spec.rb
+++ spec/controllers/twitter_controller_spec.rb
@@ -3,19 +3,39 @@ require 'spec_helper'
 
 RSpec.describe TwitterController, :type => :controller do
 
-  # need to create helper that authenticates user before these tests are run
+  before do
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+  end
 
-  describe "GET new" do
+  describe "GET #new" do
     it "renders the :new template" do
+
+      user = User.create
+      user.providers.create
+      session[:user_id] = user.id
+
       get :new
       expect(response).to render_template(:new)
     end
+
+    it "returns an error for unauthenticated users" do
+      get :new
+      expect(response).to eq 404 # redirect to root (301? 304?)
+      # expect response to redirect to PATH
+    end
   end
 
-  describe "GET show" do
+  describe "GET #show" do
+
     it "renders the :show template" do
       get :show
       expect(response).to render_template(:show)
     end
+
+    it "returns an error for unauthenticated users" do
+      get :show
+      expect(response).to eq 404
+    end
+
   end
 end
