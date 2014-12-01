db0893ea08f3bcd7c7f69acb01a4a8e30ce0b0dd
Kristen McCabe <kmccabe3@gmail.com>
2014-12-01 14:25:13 -0800

    change 'should' to 'expect' to avoid deprecation warning

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,7 +5,7 @@ describe SessionsController do
   describe "#destroy" do
     # rachel
     it "successfully logs out a user" do
-      session[:uid].should be_nil
+      expect(session[:uid]).to be_nil
     end
   end
 
