90237dbf52f02443843a6567c384380eea0c83e0
Kristen McCabe <kmccabe3@gmail.com>
2014-11-30 23:18:57 -0800

    add small changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,12 +16,13 @@ class HomeController < ApplicationController
       @client.user_timeline(feed.uid.to_i).each do |tweet|
         # try find_or_create_by(uid: blahblahid)
         #if tweet.id.nil?
-        feed.posts.create(author_name: tweet.user.name,
-                          author_handle: tweet.user.handle,
-                          author_profile_pic: tweet.user.profile_image_uri.to_s,
-                          content: tweet.text,
-                          uid: tweet.id,
-                          posted_time: tweet.created_at
+        feed.posts.find_or_create_by(
+          author_name: tweet.user.name,
+          author_handle: tweet.user.handle,
+          author_profile_pic: tweet.user.profile_image_uri.to_s,
+          content: tweet.text,
+          uid: tweet.id,
+          posted_time: tweet.created_at
         )
         #end
         end
